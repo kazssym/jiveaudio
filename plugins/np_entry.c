d7f6cf87f4b9ba9f0b3622a031debbaad7a4c1b4
kazssym <none@none>
2006-11-23 23:24:37 +0900

    (empty commit message)

--- plugins/np_entry.c
+++ plugins/np_entry.c
@@ -40,6 +40,10 @@
 // Main plugin entry point implementation -- exports from the 
 // plugin library
 //
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "npplat.h"
 
 NPNetscapeFuncs NPNFuncs;
--- plugins/npn_gate.c
+++ plugins/npn_gate.c
@@ -40,6 +40,10 @@
 //
 // Implementation of Netscape entry points (NPN_*)
 //
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "npplat.h"
 
 extern NPNetscapeFuncs NPNFuncs;
