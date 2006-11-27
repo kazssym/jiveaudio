69f0fba375caea8a3ab513c32e1575eb1119dab5
kazssym <none@none>
2006-11-28 00:53:51 +0900

    (empty commit message)

--- sdk/nptypes.h
+++ sdk/nptypes.h
@@ -41,7 +41,7 @@
  * available.
  */
 
-#if defined(WIN32) || defined(OS2)
+#if defined(WIN32) || defined(_WIN32) || defined(OS2)
   /*
    * Win32 and OS/2 don't know C99, so define [u]int_32 here. The bool
    * is predefined tho, both in C and C++.
