7068e0e93b6c140469b4d286f5b14fbccba18797
kazssym <none@none>
2007-01-30 22:19:41 +0900

    (empty commit message)

--- plugins/debug.c
+++ plugins/debug.c
@@ -1,37 +1,45 @@
-/* JiveAudio - multimedia plugin for Mozilla
-   Copyright (C) 2003 Hypercore Software Design, Ltd.
-
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307, USA.  */
+/*
+ * JiveAudio - multimedia plugin for Mozilla -*-C++-*-
+ * Copyright (C) 2003-2006 Hypercore Software Design, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
 
 #if HAVE_CONFIG_H
-#include "config.h"
+#include <config.h>
 #endif
+
 #define _GNU_SOURCE 1
 #define _REENTRANT 1
 
-#if _WIN32
+#if _WIN32                      /* Win32 or Win64 */
 #define STRICT 1
 #include <windows.h>
-#pragma hdrstop
 #endif
+#pragma hdrstop
 
-#include <stdio.h>
+#include "debug.h"
+#pragma package (smart_init)
+
 #include <stdarg.h>
+#include <stdio.h>
 
 #if !HAVE_SYSLOG_H
+
+#pragma argsused
 void syslog (int prio, const char *format,...)
 {
     va_list args;
@@ -51,5 +59,5 @@ void syslog (int prio, const char *format,...)
 
     va_end (args);
 }
-#endif /* !HAVE_SYSLOG_H */
 
+#endif /* !HAVE_SYSLOG_H */
