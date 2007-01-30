88579b3ac53656a4436563fc97eed1fe514297dd
kazssym <none@none>
2007-01-30 23:14:15 +0900

    (empty commit message)

--- plugins/player.cpp
+++ plugins/player.cpp
@@ -1,45 +1,47 @@
-/* JiveAudio - multimedia plugin for Mozilla
-   Copyright (C) 2003 Hypercore Software Design, Ltd.
-
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307, USA.  */
+/*
+ * JiveAudio - multimedia plugin for Mozilla -*-C++-*-
+ * Copyright (C) 2003-2006 Hypercore Software Design, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
 
 #if HAVE_CONFIG_H
-#include "config.h"
+#include <config.h>
 #endif
+  
+#if _WIN32                      /* Win32 or Win64 */
+#include <windows.h>
+#endif
+
 #define _GNU_SOURCE 1
 #define _REENTRANT 1
 
-#if _WIN32
-#define STRICT 1
-#include <windows.h>
-#pragma hdrstop
-#endif
+#include "player.h"
+#pragma package (smart_init)
 
-#include "media_player.h"
+player::player ()
+{
+    a_loop = false;
+}
 
-media_player::media_player () :
-    loop (false)
+player::~player ()
 {
 }
 
-media_player::~media_player ()
-{
-}
-
-void
-media_player::set_window (window_type window)
+#pragma argsused
+void player::set_window (window_type window)
 {
 }
--- plugins/player.h
+++ plugins/player.h
@@ -1,77 +1,66 @@
-/* JiveAudio - multimedia plugin for Mozilla -*-C++-*-
-   Copyright (C) 2003 Hypercore Software Design, Ltd.
+/*
+ * JiveAudio - multimedia plugin for Mozilla -*-C++-*-
+ * Copyright (C) 2003-2006 Hypercore Software Design, Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+#ifndef playerH
+#define playerH 1
 
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307, USA.  */
-
-#ifndef _MEDIA_PLAYER_H
-#define _MEDIA_PLAYER_H 1
-
-#include <cstdio>
-
-#if !_WIN32
-#include <X11/Intrinsic.h>
-#endif
-
-class media_player
-{
+/**
+ * Abstract media player.
+ */
+class player {
 public:
 #if _WIN32
     typedef HWND window_type;
 #else
-    typedef Widget window_type;
+    typedef void *window_type;
 #endif
-public:
-    media_player ();
-    virtual ~media_player ();
 
-    void set_loop (bool loop);
+public:
+    player ();
+
+public:
+    virtual ~player ();
+
+public:
+    void set_loop (bool loop)
+    {
+        a_loop = loop;
+    }
+    bool loop () const
+    {
+        return a_loop;
+    }
+protected:
+    bool a_loop;
+
+public:
     virtual bool open_stream (const char *mime_type) = 0;
     virtual size_t stream_buffer_size () const = 0;
     virtual size_t write_stream (const void *buf, size_t nbytes) = 0;
     virtual void close_stream () = 0;
+
+public:
     virtual void start () = 0;
     virtual void stop () = 0;
 
     virtual void set_window (window_type window);
-protected:
-    bool loop;
 };
 
-inline void
-media_player::set_loop (bool loop)
-{
-    this->loop = loop;
-}
-
-class file_media_player : public media_player
-{
-public:
-    file_media_player ();
-    ~file_media_player ();
-
-    bool open_stream (const char *mime_type);
-    size_t stream_buffer_size () const;
-    size_t write_stream (const void *buf, size_t nbytes);
-    void close_stream ();
-protected:
-    char *file_name;
-private:
-    int fildes;
-
-    void clean ();
-};
-
-#endif /* !_MEDIA_PLAYER_H */
+#endif
